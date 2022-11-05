c3b31924b8185ddcd9b4781f3325cbb371469877
dmitry grunt <dmitry.grunt@protonmail.com>
2022-11-05 19:18:10 +0200

    Create new order button fix

--- src/auth/check-jwt.js
+++ src/auth/check-jwt.js
@@ -1,23 +1,19 @@
-// const jwt = require('express-jwt');
+var express = require("express");
+var app = express();
+// var jwt = require("express-jwt"); Error
 const { expressjwt: jwt } = require("express-jwt");
-const jwksRsa = require("jwks-rsa");
-const { auth } = require("express-oauth2-jwt-bearer");
-
-const appOrigin = process.env.APP_ORIGIN;
-const audience = process.env.AUTH0_AUDIENCE;
-const issuer = process.env.AUTH0_ISSUER;
+var jwks = require("jwks-rsa");
 
 const checkJwt = jwt({
-    secret: jwksRsa.expressJwtSecret({
+    secret: jwks.expressJwtSecret({
         cache: true,
         rateLimit: true,
         jwksRequestsPerMinute: 5,
-        jwksUri: `${issuer}.well-known/jwks.json`,
-      }),
-    
-      audience: audience,
-      issuer: issuer,
-      algorithms: ["RS256"],
+        jwksUri: "https://dev-n023tzazgiffxens.eu.auth0.com/.well-known/jwks.json",
+    }),
+    audience: "https://dashboard.dimenshteyn.co.il/",
+    issuer: "https://dev-n023tzazgiffxens.eu.auth0.com/",
+    algorithms: ["RS256"],
 });
 
 module.exports = {
