595bc1ff299ce06bb45692a514fbd54c51e0de41
dmitry grunt <dmitry.grunt@protonmail.com>
2022-11-06 15:04:21 +0200

    fix

--- src/routes/v1/routes/account.js
+++ src/routes/v1/routes/account.js
@@ -5,6 +5,7 @@ const jwt = require("jsonwebtoken");
 const prisma = require("../../../utils/prisma");
 const JWT_SECRET = process.env.JWT_SECRET;
 const JWT_EXPIRES_TIME = process.env.JWT_ACCESS_EXPIRATION_MINUTES;
+const { checkJwt } = require("./../../../auth/check-jwt");
 
 router
     .post("/login", async function (req, res, next) {
